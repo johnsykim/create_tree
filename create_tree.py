eed1fc3cbbf796f291d55212abb01a5526d9e7f7
johnsykim <sababi0314@gmail.com>
2020-10-21 03:02:20 +0000

    Added welcome statement and program abortion feature

--- create_tree.py
+++ create_tree.py
@@ -17,3 +17,43 @@
 
   Every time the program runs, it updates 'metadata_history.txt', which contains the root directory, program run date, and the user-defined parameters.
 """
+
+# Import packages and define template_code and template_item
+import os, shutil
+pwd = os.getcwd()
+template_code = pwd/'template_code.py'
+template_item = ['0_main.py', '1_task.py', '2_task.py']
+
+# Override input function so that the user can abort the program at anytime
+__original_input = input
+def input(*args, **kwargs):
+  res = __original_input(*args, **kwargs)
+  if res == "q":
+    print("aborting the program...\n"+"********************************************")
+    exit()
+  else:
+    return res
+
+# Welcome statement
+print("*********************************************************************\n"
+  + "Welcome! You're about to create your new project.\n"
+  + "Before you proceed, you'll need the following information:\n"
+  + "- Desired working directory\n"
+  + "- Project name\n"
+  + "- # of collaborators and their IDs\n"
+  + "- # of subprojects\n"
+  + "To quit this program at anytime, type \"q\" and press Enter \n"
+  + "*********************************************************************")
+
+# Ask if the user is ready
+while True:
+  ready = input("Are you ready? [y/n] ")
+  if ready == 'y':
+    print("Let's begin.")
+    break
+  elif ready == 'n':
+    print("That's okay. I'll see you later.")
+    exit()
+  else:
+    print("Please type either \"y\" or \"n\".")
+    continue
